472651655b4e1ce991ebde27feee911e1c726f2c
MicroWin10-2212\Administrator <954274576@qq.com>
2018-08-06 21:25:31 +0800

    获取文件列表

--- src_cgi/myfiles_cgi.c
+++ src_cgi/myfiles_cgi.c
@@ -52,8 +52,25 @@ int main()
 			}
 			//处理数据
 
-           //循环获取这个用户的文件列表
-            get_file_list(uid);
+             //解析json
+            cJSON * json_object= cJSON_Parse(receivData);//从 给定的json字符串中得到cjson对象
+
+            //从json对象中获取值
+            cJSON   * action_json=cJSON_GetObjectItem(json_object,"action");
+
+            char * action=cJSON_Print(action_json);
+
+
+            LOG(LOG_MODULE, LOG_PROC, "LOG:%s\n",action);
+
+            //如果是获取文件列表
+            if(strcmp(action,"\"get_list\"")==0){
+
+                //循环获取这个用户的文件列表
+                 get_file_list(uid);
+            }
+
+          
 
 
             //返回json
@@ -103,9 +120,36 @@ void get_file_list(int uid){
     //生成结果集
     MYSQL_RES *res_set = mysql_store_result(mysql_conn); 
 
- 
-    //处理结果集
-    process_result_test(mysql_conn,res_set);
+
+    MYSQL_ROW row;
+    uint i;
+
+    
+    cJSON * root =  cJSON_CreateArray();
+    
+    //获取列属性
+    MYSQL_FIELD *fields;
+
+    //从结果集中抓取列属性
+    fields = mysql_fetch_fields(res_set);
 
 
+    // mysql_fetch_row从使用mysql_store_result得到的结果结构中提取一行，并把它放到一个行结构中。当数据用完或发生错误时返回NULL.
+    while ((row = mysql_fetch_row(res_set)) != NULL)
+    {
+        //每次都重新开辟空间,这样添加到的才会不是同一个地址，才能不添加到一起去
+        cJSON * result =  cJSON_CreateObject();
+
+        //mysql_num_fields获取结果中列的个数
+        for(i = 0; i < mysql_num_fields(res_set); i++)
+        {
+            //把键值对添加到result对象中
+            cJSON_AddItemToObject(result,fields[i].name, cJSON_CreateString(row[i]));
+        }
+
+        //把对象加入到根节点
+        cJSON_AddItemToArray(root,result);
+    }
+
+     printf("%s\n", cJSON_Print(root));
 }
